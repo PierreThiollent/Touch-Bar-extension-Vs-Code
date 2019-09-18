787edd82c881cb9114d2f28e6074c2efdd32c2aa
Pierre Thiollent <pierre.thiollent76@gmail.com>
2019-09-18 10:44:11 +0200

    Ajout de raccourcis pour indenter le PHP et pour ajouter un nouveau fichier. Ajout des icônes pour la Touch Bar

--- src/extension.ts
+++ src/extension.ts
@@ -14,28 +14,26 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 
+	// Commande pour commenter ou décommenter une ligne
+	let commentLine = vscode.commands.registerCommand('extension.commentLine', () => {
+		vscode.commands.executeCommand('editor.action.commentLine');
+	});
+
 	// On définit une commande pour Beautify
 	let beautify = vscode.commands.registerCommand('extension.Beautify', () => {
 		vscode.commands.executeCommand('HookyQR.beautify');
 	});
 
 	// On définit une commande pour Php fmt
-	// let phpfmt = vscode.commands.registerCommand('extension.phpfmt', () => {
-	// 	vscode.commands.executeCommand('HookyQR.beautify');
-	// });
-
-	// Commande pour commenter une ligne
-	let commentLine = vscode.commands.registerCommand('extension.commentLine', () => {
-		vscode.commands.executeCommand('editor.action.addCommentLine');
+	let phpfmt = vscode.commands.registerCommand('extension.phpfmt', () => {
+		vscode.commands.executeCommand('phpfmt.format');
 	});
 
-	// Commande pour decommenter une ligne
-	let removeCommentLine = vscode.commands.registerCommand('extension.removeCommentLine', () => {
-		vscode.commands.executeCommand('editor.action.removeCommentLine');
+	let newFile = vscode.commands.registerCommand('extension.newFile', () => {
+		vscode.commands.executeCommand('explorer.newFile');
 	});
 
-
-	context.subscriptions.push(beautify, commentLine, removeCommentLine);
+	context.subscriptions.push(commentLine, beautify, phpfmt, newFile);
 }
 
 // this method is called when your extension is deactivated
